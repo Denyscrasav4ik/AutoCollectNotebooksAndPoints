827c3109cd4c038dddb1d27d7a2b448971de9262
Denyscrasav4ik <horanden238@gmail.com>
2026-09-09 19:37:43 +0300

    Initial Commit/Release

---
+++ AutoCollectNotebooksAndPoints/AutoCollectNotebooksAndPoints/PickupPlugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace AutoCollectNotebooksAndPoints;
+
+[BepInPlugin("denyscrasav4ik.thedumbfactory.autocollectnotebooksandpoints", "Auto Collect Notebooks and Points", "1.0.0")]
+public class PickupPlugin : BaseUnityPlugin
+{
+    private void Awake() => new Harmony("denyscrasav4ik.thedumbfactory.autocollectnotebooksandpoints").PatchAll();
+}
+
+[HarmonyPatch(typeof(Notebook), "ClickableSighted")]
+public class NotebookPatch
+{
+    internal static void Postfix(Notebook __instance, int player)
+    {
+        if (!__instance.collected)
+            __instance.Clicked(player);
+    }
+}
+
+[HarmonyPatch(typeof(Pickup), "ClickableSighted")]
+public class PickupPatch
+{
+    internal static void Postfix(Pickup __instance, int player)
+    {
+        if (__instance.item != null && __instance.item.item is ITM_YTPs)
+            __instance.Clicked(player);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Baldi's Basics Plus Auto Collect Notebooks and Points
+
+## 📝 Functionality
+
+Basically, this mod automatically collects notebooks and points once you hover over them.
+
+## 📜 License
+
+This project is shared under the MIT License. Read LICENSE for more info.
